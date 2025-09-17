dcfd1a5e98da112470db86128510e71adf182cd6
munaibrahimn <munaa97@gmail.com>
2025-09-17 14:48:18 +0100

    settings for insights hardcoded

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,19 +110,19 @@ module "vpc" {
 module "ecs" {
   source = "./modules/ecs_fargate"
 
-  cluster_name          = var.cluster_name
-  service_name          = var.service_name
+  cluster_name = var.cluster_name
+  service_name = var.service_name
   # cluster_insight_name  = var.cluster_insight_name
   # cluster_insight_value = var.cluster_insight_value
-  desired_count         = var.desired_count
-  container_name        = var.container_name
-  container_port        = var.container_port
-  task_family           = var.task_family
-  task_cpu              = var.task_cpu
-  task_memory           = var.task_memory
-  image_url             = var.image_url
-  execution_role_arn    = module.iam_roles.execution_role_arn
-  target_group_arn      = module.alb.target_group_arn
-  subnet_ids            = module.vpc.private_subnet_ids
-  security_group_ids    = [module.security_groups.ecs_sg_id]
+  desired_count      = var.desired_count
+  container_name     = var.container_name
+  container_port     = var.container_port
+  task_family        = var.task_family
+  task_cpu           = var.task_cpu
+  task_memory        = var.task_memory
+  image_url          = var.image_url
+  execution_role_arn = module.iam_roles.execution_role_arn
+  target_group_arn   = module.alb.target_group_arn
+  subnet_ids         = module.vpc.private_subnet_ids
+  security_group_ids = [module.security_groups.ecs_sg_id]
 }
--- terraform/modules/ecs_fargate/main.tf
+++ terraform/modules/ecs_fargate/main.tf
@@ -14,7 +14,7 @@ resource "aws_ecs_cluster" "this" {
   setting {
     name  = "containerInsights"
     value = "enabled"
-}
+  }
 }
 
 resource "aws_ecs_service" "this" {
