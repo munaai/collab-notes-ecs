25b69a17994f954fb8507917fedd9c0918d2fdf4
munaibrahimn <munaa97@gmail.com>
2025-09-21 15:36:39 +0100

    update

--- terraform/modules/iam_roles/outputs.tf
+++ terraform/modules/iam_roles/outputs.tf
@@ -1,7 +1,23 @@
-output "ecs_execution_role_arn" {
-  value = length(aws_iam_role.ecs_execution) > 0 ? aws_iam_role.ecs_execution[0].arn : null
+# ECS Execution Role ARN
+output "execution_role_arn" {
+  description = "ARN of the ECS execution role"
+  value       = var.create_ecs_execution_role ? aws_iam_role.ecs_execution[0].arn : null
 }
 
+# ECS Execution Role Name
+output "execution_role_name" {
+  description = "Name of the ECS execution role"
+  value       = var.create_ecs_execution_role ? aws_iam_role.ecs_execution[0].name : null
+}
+
+# Flow Logs Role ARN
 output "flow_logs_role_arn" {
-  value = length(aws_iam_role.flow_logs) > 0 ? aws_iam_role.flow_logs[0].arn : null
+  description = "ARN of the Flow Logs role"
+  value       = var.create_flow_logs_role ? aws_iam_role.flow_logs[0].arn : null
+}
+
+# Flow Logs Role Name
+output "flow_logs_role_name" {
+  description = "Name of the Flow Logs role"
+  value       = var.create_flow_logs_role ? aws_iam_role.flow_logs[0].name : null
 }
