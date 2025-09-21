c611b81d6780bc236c2d6bebda483885c1021a56
munaibrahimn <munaa97@gmail.com>
2025-09-21 18:16:42 +0100

    alb updated

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -164,8 +164,9 @@ resource "aws_wafv2_web_acl_logging_configuration" "alb_waf_logging" {
   count = var.enable_waf ? 1 : 0
 
   log_destination_configs = [
-    "${aws_cloudwatch_log_group.waf_logs.arn}:*"
+    aws_cloudwatch_log_group.waf_logs.log_group_arn
   ]
+
 
   resource_arn = aws_wafv2_web_acl.alb_waf[0].arn
 }
