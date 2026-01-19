e1e3ccbaec1d2eec1098b4caff41cb92114ae224
munaibrahimn <munaa97@gmail.com>
2026-01-19 18:31:12 +0000

    bootstrap changes made

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-terraform-config-bucket-muna"
     key            = "bootstrap/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "terraform-state-locks"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
---
+++ bootstrap/main.tf
@@ -0,0 +1,54 @@
+resource "aws_ecr_repository" "app" {
+  name = "collab-notes-app"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:munaai/collab-notes-ecs:*"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-terraform"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
